beca67ffbb0af77699be19799aa176ce51660c26
Candy Wang <candywang013@gmail.com>
2019-10-23 12:04:05 -0700

    Implement static path to public directory

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  res.end();
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
